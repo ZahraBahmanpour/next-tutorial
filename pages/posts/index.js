6c24320f79078cfae58b452d7da022a1c570305e
ZahraBahmanpour <z.bmnpr@gmail.com>
2022-08-30 12:37:13 +0430

    add getStaticProps to posts

--- pages/posts/index.js
+++ pages/posts/index.js
@@ -1,6 +1,6 @@
 import { useRouter } from "next/router";
 
-export default function Posts() {
+export default function Posts({ posts }) {
   const router = useRouter();
   const handleClick = () => {
     router.push("/");
@@ -8,7 +8,25 @@ export default function Posts() {
   return (
     <>
       <h1>Posts</h1>
+      <ul>
+        {posts.map((post) => (
+          <li key={post.id}>{post.title}</li>
+        ))}
+      </ul>
       <h3 onClick={handleClick}>Back</h3>
     </>
   );
+}
+
+export async function getStaticProps() {
+  const response = await fetch(
+    "https://6300a18859a8760a757d441c.mockapi.io/posts"
+  );
+  const posts = await response.json();
+
+  return {
+    props: {
+      posts,
+    },
+  };
 }
