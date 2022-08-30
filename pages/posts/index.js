7cf2665a9565ef13cd320739449c65115d8a72e2
ZahraBahmanpour <z.bmnpr@gmail.com>
2022-08-30 12:19:59 +0430

    navigate programmatically

--- pages/posts/index.js
+++ pages/posts/index.js
@@ -1,3 +1,14 @@
+import { useRouter } from "next/router";
+
 export default function Posts() {
-  return <h1>Posts</h1>;
+  const router = useRouter();
+  const handleClick = () => {
+    router.push("/");
+  };
+  return (
+    <>
+      <h1>Posts</h1>
+      <h3 onClick={handleClick}>Back</h3>
+    </>
+  );
 }
