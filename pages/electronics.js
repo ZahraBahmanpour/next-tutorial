0f4106809bd6b8dac8d07a76bdacfae7e2c98271
ZahraBahmanpour <z.bmnpr@gmail.com>
2022-08-31 14:09:19 +0430

    add images and Image component to electronics

---
+++ pages/electronics.js
@@ -0,0 +1,18 @@
+import Image from "next/image";
+
+export default function ElectronicsPage() {
+  return (
+    <div>
+      {["1", "2", "3", "4"].map((path) => (
+        <div key={path}>
+          <Image
+            src={`/images/${path}.jpg`}
+            height={300}
+            width={300}
+            alt={"electronics"}
+          />
+        </div>
+      ))}
+    </div>
+  );
+}
